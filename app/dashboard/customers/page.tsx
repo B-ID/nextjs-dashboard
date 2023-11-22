50b15b474f1c61cafd10706ccd36844f6e33e0f0
olubukola@zestpayment.com <olubukola@zestpayment.com>
2023-11-22 17:45:12 +0100

    first push

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>customers page</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div>Dashboard page</div>
+  )
+}
+
+export default Page
