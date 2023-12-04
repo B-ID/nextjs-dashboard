f7414ab9f7315830a8eb23e0cbbe05bb47c083ac
olubukola@zestpayment.com <olubukola@zestpayment.com>
2023-12-04 11:43:16 +0100

    pagination search loading screens and create invoice functionality

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 type Props = {}
 
 const Page = (props: Props) => {
+  const {} = props
   return (
     <div>customers page</div>
   )
---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,40 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { FC } from "react";
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+
+const Page: FC<Props> = async (props) => {
+  const {
+    params: { id },
+  } = props;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) notFound()
+
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+};
+
+export default Page;
